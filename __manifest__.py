a45c4ea7514c580d3bafa7a877423b2eec58acba
mgah77 <mauricio.gah@gmail.com>
2026-04-20 16:39:39 -0400

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-{ 'name': 'HR Prestamos',
-'summary': "Prestamos",
+{ 'name': 'RRHH Insumar',
+'summary': "RRHH",
 'author': "Mauricio Gah",
 'license': "AGPL-3",
 'application': "True",
