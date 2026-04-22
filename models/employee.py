9380aaa0484010022ce6b0af66ca99fe5a2d2a60
mgah77 <138524599+mgah77@users.noreply.github.com>
2026-04-22 08:52:26 -0400

    wr

--- models/employee.py
+++ models/employee.py
@@ -52,7 +52,7 @@ class ParcheHR(models.Model):
         ('parcial','Parcial')], 'Jornada Laboral')
     horas = fields.Integer('Horas Semanales')
 
-    banco = fields.Many2One('Banco', store='True')
+    #banco = fields.Many2One('Banco', store='True')
     cuenta_banco = fields.Integer(string='Cuenta')
     cuenta_tipo = fields.Selection([
         ('cc','Cuenta Corriente'),
