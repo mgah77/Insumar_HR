95de8be48212e0e7798308f5e96436482e1f675d
mgah77 <138524599+mgah77@users.noreply.github.com>
2026-05-15 10:16:32 -0400

    Update employee.py

--- models/employee.py
+++ models/employee.py
@@ -52,7 +52,8 @@ class ParcheHR(models.Model):
         ('parcial','Parcial')], 'Jornada Laboral')
     horas = fields.Integer('Horas Semanales')
 
-    #banco = fields.Many2One('Banco', store='True')
+    banco = fields.Many2One('hr.employee.bank.rel', string = 'Banco')
+    lugar = fields.Many2one('taller.lugar.rel', string = 'Lugar')
     cuenta_banco = fields.Integer(string='Cuenta')
     cuenta_tipo = fields.Selection([
         ('cc','Cuenta Corriente'),
